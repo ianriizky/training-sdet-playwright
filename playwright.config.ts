88ec2b28c283367b55ca2a91cb6167fb297b01a6
Septianata Rizky Pratama <hi@ianrizky.web.id>
2025-11-13 05:06:00 +0700

    chore: increase local Playwright workers from default to 4

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  ...(process.env.CI ? { workers: 1 } : {}),
+  ...(process.env.CI ? { workers: 1 } : { workers: 4 }),
   reporter: [
     ['html'],
     ['line'],
